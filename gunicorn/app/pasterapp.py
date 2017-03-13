04ff969b92d81d15dc63c6ccf2b64de068511a97
Randall Leeds <randall@bleeds.info>
2017-03-12 17:14:36 -0700

    Simplify paste integration
    Simplify the paste integration.
    
    The `--paste` option still works, but requires `plaster` and does not
    support parsing the server section of the config file. Instead, server
    configuration must be done with Gunicorn configuration files, command
    line switches, and environment variables.
    
    The use of Gunicorn as a paste server factory still works. It allows
    specifying `host` and `port` or `listen` options, as well as `bind`, but
    is otherwise more strict with options. Rather than ignoring unknown
    options it will raise an error. There is no support for using a Gunicorn
    config file when using a paste runner. Instead, all options must be
    specified in the paste configuration file. No manipulation of the system
    module path is performed.

--- gunicorn/app/pasterapp.py
+++ gunicorn/app/pasterapp.py
@@ -3,206 +3,68 @@
 # This file is part of gunicorn released under the MIT license.
 # See the NOTICE for more information.
 
-# pylint: skip-file
+import configparser
 
-import os
-import pkg_resources
-import sys
-
-try:
-    import configparser as ConfigParser
-except ImportError:
-    import ConfigParser
-
-from paste.deploy import loadapp, loadwsgi
-SERVER = loadwsgi.SERVER
-
-from gunicorn.app.base import Application
-from gunicorn.config import Config, get_default_config_file
-from gunicorn import util
+from gunicorn.app.wsgiapp import WSGIApplication
 
 
-def _has_logging_config(paste_file):
-    cfg_parser = ConfigParser.ConfigParser()
-    cfg_parser.read([paste_file])
-    return cfg_parser.has_section('loggers')
-
-
-def paste_config(gconfig, config_url, relative_to, global_conf=None):
-    # add entry to pkg_resources
-    sys.path.insert(0, relative_to)
-    pkg_resources.working_set.add_entry(relative_to)
-
-    config_url = config_url.split('#')[0]
-    cx = loadwsgi.loadcontext(SERVER, config_url, relative_to=relative_to,
-                              global_conf=global_conf)
-    gc, lc = cx.global_conf.copy(), cx.local_conf.copy()
-    cfg = {}
-
-    host, port = lc.pop('host', ''), lc.pop('port', '')
-    if host and port:
-        cfg['bind'] = '%s:%s' % (host, port)
-    elif host:
-        cfg['bind'] = host.split(',')
-
-    cfg['default_proc_name'] = gc.get('__file__')
-
-    # init logging configuration
-    config_file = config_url.split(':')[1]
-    if _has_logging_config(config_file):
-        cfg.setdefault('logconfig', config_file)
-
-    for k, v in gc.items():
-        if k not in gconfig.settings:
-            continue
-        cfg[k] = v
-
-    for k, v in lc.items():
-        if k not in gconfig.settings:
-            continue
-        cfg[k] = v
-
-    return cfg
-
-
-def load_pasteapp(config_url, relative_to, global_conf=None):
-    return loadapp(config_url, relative_to=relative_to,
-            global_conf=global_conf)
-
-class PasterBaseApplication(Application):
-    gcfg = None
-
-    def app_config(self):
-        return paste_config(self.cfg, self.cfgurl, self.relpath,
-                global_conf=self.gcfg)
-
-    def load_config(self):
-        super(PasterBaseApplication, self).load_config()
-
-        # reload logging conf
-        if hasattr(self, "cfgfname"):
-            parser = ConfigParser.ConfigParser()
-            parser.read([self.cfgfname])
-            if parser.has_section('loggers'):
-                from logging.config import fileConfig
-                config_file = os.path.abspath(self.cfgfname)
-                fileConfig(config_file, dict(__file__=config_file,
-                                             here=os.path.dirname(config_file)))
-
-
-class PasterApplication(PasterBaseApplication):
-
+class PasterApplication(WSGIApplication):
     def init(self, parser, opts, args):
         if len(args) != 1:
-            parser.error("No application name specified.")
+            parser.error("No configuration source specified.")
 
-        cwd = util.getcwd()
-        cfgfname = os.path.normpath(os.path.join(cwd, args[0]))
-        cfgfname = os.path.abspath(cfgfname)
-        if not os.path.exists(cfgfname):
-            parser.error("Config file not found: %s" % cfgfname)
+        opts.paste = args.pop()
 
-        self.cfgurl = 'config:%s' % cfgfname
-        self.relpath = os.path.dirname(cfgfname)
-        self.cfgfname = cfgfname
-
-        sys.path.insert(0, self.relpath)
-        pkg_resources.working_set.add_entry(self.relpath)
-
-        return self.app_config()
-
-    def load(self):
-        # chdir to the configured path before loading,
-        # default is the current dir
-        os.chdir(self.cfg.chdir)
-
-        return load_pasteapp(self.cfgurl, self.relpath, global_conf=self.gcfg)
+        return super().init(parser, opts, args)
 
 
-class PasterServerApplication(PasterBaseApplication):
+class PasterServerApplication(WSGIApplication):
+    def __init__(self, app, global_conf, local_conf):
+        super().__init__()
+        self.callable = app
+        for k, v in normalize_config(global_conf, local_conf).items():
+            self.cfg.set(k, v)
 
-    def __init__(self, app, gcfg=None, host="127.0.0.1", port=None, **kwargs):
-        # pylint: disable=super-init-not-called
-        self.cfg = Config()
-        self.gcfg = gcfg  # need to hold this for app_config
-        self.app = app
-        self.callable = None
-
-        gcfg = gcfg or {}
-        cfgfname = gcfg.get("__file__")
-        if cfgfname is not None:
-            self.cfgurl = 'config:%s' % cfgfname
-            self.relpath = os.path.dirname(cfgfname)
-            self.cfgfname = cfgfname
-
-        cfg = kwargs.copy()
-
-        if port and not host.startswith("unix:"):
-            bind = "%s:%s" % (host, port)
-        else:
-            bind = host
-        cfg["bind"] = bind.split(',')
-
-        if gcfg:
-            for k, v in gcfg.items():
-                cfg[k] = v
-            cfg["default_proc_name"] = cfg['__file__']
-
-        try:
-            for k, v in cfg.items():
-                if k.lower() in self.cfg.settings and v is not None:
-                    self.cfg.set(k.lower(), v)
-        except Exception as e:
-            print("\nConfig error: %s" % str(e), file=sys.stderr)
-            sys.stderr.flush()
-            sys.exit(1)
-
-        if cfg.get("config"):
-            self.load_config_from_file(cfg["config"])
-        else:
-            default_config = get_default_config_file()
-            if default_config is not None:
-                self.load_config_from_file(default_config)
-
-    def load(self):
-        return self.app
+    def load_config(self):
+        pass
 
 
-def run():
+def has_logging_config(config_file):
+    parser = configparser.ConfigParser()
+    parser.read([config_file])
+    return parser.has_section('loggers')
+
+
+def normalize_config(global_conf, local_conf):
+    config = local_conf.copy()
+    config_file = global_conf['__file__']
+
+    host = config.pop('host', '')
+    port = config.pop('port', '')
+    if host and port:
+        config['bind'] = '%s:%s' % (host, port)
+    elif host:
+        config['bind'] = host.split(',')
+
+    listen = config.pop('listen', '')
+    if listen:
+        config['bind'] = listen
+
+    if has_logging_config(config_file):
+        config.setdefault('logconfig', config_file)
+
+    return config
+
+
+def serve(app, global_conf, **local_conf):
     """\
-    The ``gunicorn_paster`` command for launching Paster compatible
-    applications like Pylons or Turbogears2
+    A paste server runner.
+
+    Example configuration:
+
+        [server:main]
+        use = egg:gunicorn#main
+        host = 127.0.0.1
+        port = 5000
     """
-    util.warn("""This command is deprecated.
-
-    You should now use the `--paste` option. Ex.:
-
-        gunicorn --paste development.ini
-    """)
-
-    from gunicorn.app.pasterapp import PasterApplication
-    PasterApplication("%(prog)s [OPTIONS] pasteconfig.ini").run()
-
-
-def paste_server(app, gcfg=None, host="127.0.0.1", port=None, **kwargs):
-    """\
-    A paster server.
-
-    Then entry point in your paster ini file should looks like this:
-
-    [server:main]
-    use = egg:gunicorn#main
-    host = 127.0.0.1
-    port = 5000
-
-    """
-
-    util.warn("""This command is deprecated.
-
-    You should now use the `--paste` option. Ex.:
-
-        gunicorn --paste development.ini
-    """)
-
-    from gunicorn.app.pasterapp import PasterServerApplication
-    PasterServerApplication(app, gcfg=gcfg, host=host, port=port, **kwargs).run()
+    PasterServerApplication(app, global_conf, local_conf).run()
--- gunicorn/app/wsgiapp.py
+++ gunicorn/app/wsgiapp.py
@@ -3,53 +3,36 @@
 # This file is part of gunicorn released under the MIT license.
 # See the NOTICE for more information.
 
-import os
-
-from gunicorn.errors import ConfigError
 from gunicorn.app.base import Application
 from gunicorn import util
 
 
 class WSGIApplication(Application):
     def init(self, parser, opts, args):
+        config = {}
+
         if opts.paste:
-            app_name = 'main'
-            path = opts.paste
-            if '#' in path:
-                path, app_name = path.split('#')
-            path = os.path.abspath(os.path.normpath(
-                os.path.join(util.getcwd(), path)))
-
-            if not os.path.exists(path):
-                raise ConfigError("%r not found" % path)
-
-            # paste application, load the config
-            self.cfgurl = 'config:%s#%s' % (path, app_name)
-            self.relpath = os.path.dirname(path)
-
-            from .pasterapp import paste_config
-            return paste_config(self.cfg, self.cfgurl, self.relpath)
+            import plaster
+            loader = plaster.get_loader(opts.paste, protocols=['wsgi'])
+            config['default_proc_name'] = str(loader.uri)
+            if 'loggers' in loader.get_sections():
+                config['logconfig'] = loader.uri.path
+            return config
 
         if not args:
             parser.error("No application module specified.")
 
-        self.cfg.set("default_proc_name", args[0])
-        self.app_uri = args[0]
+        config['default_proc_name'] = args[0]
 
-    def load_wsgiapp(self):
-        # load the app
-        return util.import_app(self.app_uri)
-
-    def load_pasteapp(self):
-        # load the paste app
-        from .pasterapp import load_pasteapp
-        return load_pasteapp(self.cfgurl, self.relpath, global_conf=self.cfg.paste_global_conf)
+        return config
 
     def load(self):
-        if self.cfg.paste is not None:
-            return self.load_pasteapp()
-        else:
-            return self.load_wsgiapp()
+        if self.cfg.paste:
+            import plaster
+            loader = plaster.get_loader(self.cfg.paste, protocols=['wsgi'])
+            return loader.get_wsgi_app()
+
+        return util.import_app(self.app_uri)
 
 
 def run():
